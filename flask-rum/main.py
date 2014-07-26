698358fcad02b611735b42c3150b565a969ecd81
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-07-25 19:07:43 -0700

    stop passing rum to everything. use the new settings import

--- flask-rum/main.py
+++ flask-rum/main.py
@@ -10,20 +10,20 @@ app.debug = True
 
 
 @app.route('/')
-def frontpage(title='Home '+rum['title']):
-    return render_template('site/frontpage.html', title=title, rum = rum)
+def frontpage(title='Home '+settings.PROJECT_TITLE):
+    return render_template('site/frontpage.html', title=title)
 
 @app.route('/about/')
 def about(title='About Us'):
-    return render_template('site/about.html', title=title, rum = rum)
+    return render_template('site/about.html', title=title)
 
 @app.route('/info/')
 def info(title='Info Page'):
-    return render_template('site/info.html', title=title, rum = rum)
+    return render_template('site/info.html', title=title)
 
 @app.route('/<path:path>')
-def catch_all(path, title='404 Error! '+rum['title']):
-    return render_template('site/404.html', title=title, path=path, rum = rum)
+def catch_all(path, title='404 Error! '+settings.PROJECT_TITLE):
+    return render_template('site/404.html', title=title, path=path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
