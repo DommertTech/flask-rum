7a43fbe7078ec9890b6107b4849ef9cc3afa013e
Dommert <DommertEnterprises@Gmail.com>
2014-08-02 02:11:45 -0700

    blueprint updates

--- runme.py
+++ runme.py
@@ -1,18 +1,27 @@
 # Dommert Flask-Rum Loader
 from flask import Flask
 from flask import render_template
-from flask_rum.main import rum
-
 app = Flask(__name__)
-app.register_blueprint(rum)
 
+#Import in Rum Configs
 import flask_rum.rum_config as rum_config
 app.config.from_object(rum_config)
+
+# Sample override of Theme
 app.config.THEME_FOLDER='rum/coconut/'
 
+# Sample Override over route('/about/')
+@app.route('/about/')
+def about2(title='Home '):
+    return render_template('site/frontpage2.html', title=title)
 
+#  Flask-Rum Blueprint
+from flask_rum.main import rum
+app.register_blueprint(rum)
+
+# Regular Route using Flask-Rum
 @app.route('/sample')
-def sample_pg(title='Home '):
+def sample(title='Home '):
     return render_template('site/frontpage2.html', title=title)
 
 if __name__ == '__main__':
