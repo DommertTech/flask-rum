385a93fd75777a79777f31a249411125d2f38340
Dommert <DommertEnterprises@Gmail.com>
2014-08-06 04:52:26 -0700

    made rum its own file

---
+++ rum.py
@@ -0,0 +1,9 @@
+# ---- RUM ----------------------------
+from runme import app
+from flask_rum.main import rum
+import flask_rum.rum_config as rum_config
+app.config.from_object(rum_config)
+# Sample override of Theme
+app.config.THEME_FOLDER='rum/banana/' # Flavor of Rum
+app.register_blueprint(rum) #  Flask-Rum Blueprint
+#------------------------------------
--- runme.py
+++ runme.py
@@ -17,16 +17,8 @@ def about2(title='Home '):
 def sample(title='Home '):
     return render_template('site/sample.html', title=title)
 
+from rum import *
 
-# ---- RUM ----------------------------
-from flask_rum.main import rum
-import flask_rum.rum_config as rum_config
-app.config.from_object(rum_config)
-# Sample override of Theme
-app.config.THEME_FOLDER='rum/banana/' # Flavor of Rum
-app.register_blueprint(rum) #  Flask-Rum Blueprint
-
-#------------------------------------
 
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
