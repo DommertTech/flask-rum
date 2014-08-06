3503f903eb7eca3750895617783b2fab4966680c
Dommert <DommertEnterprises@Gmail.com>
2014-08-06 04:47:55 -0700

    changing format

--- runme.py
+++ runme.py
@@ -3,16 +3,9 @@
 
 from flask import Flask
 from flask import render_template
-from flask_rum.main import rum
-
 app = Flask(__name__)
 
-#Import in Rum Configs
-import flask_rum.rum_config as rum_config
-app.config.from_object(rum_config)
 
-# Sample override of Theme
-app.config.THEME_FOLDER='rum/banana/'
 
 # Sample Override over route('/about/')
 @app.route('/about/')
@@ -24,6 +17,16 @@ def about2(title='Home '):
 def sample(title='Home '):
     return render_template('site/sample.html', title=title)
 
+
+# ---- RUM ----------------------------
+from flask_rum.main import rum
+import flask_rum.rum_config as rum_config
+app.config.from_object(rum_config)
+# Sample override of Theme
+app.config.THEME_FOLDER='rum/banana/' # Flavor of Rum
+app.register_blueprint(rum) #  Flask-Rum Blueprint
+
+#------------------------------------
+
 if __name__ == '__main__':
-    app.register_blueprint(rum) #  Flask-Rum Blueprint
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
