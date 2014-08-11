d3617717247e460a2f57029cfacf7d885f0f19f9
Dommert <DommertEnterprises@Gmail.com>
2014-08-11 00:50:21 -0700

    rum

--- runme.py
+++ runme.py
@@ -5,8 +5,6 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-
-
 # Sample Override over route('/about/')
 @app.route('/about/')
 def about2(title='Home '):
@@ -18,7 +16,6 @@ def sample(title='Home '):
     return render_template('site/sample.html', title=title)
 
 from rum import *
-
 
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
